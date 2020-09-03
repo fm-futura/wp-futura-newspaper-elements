405740a124a7214013c8910ef52b0d4d22cbae76
Adrián Pardini <github@tangopardo.com.ar>
2020-09-03 09:39:19 -0300

    td_block_futura_flex_1: split childs into rows of 4 modules

--- shortcodes/td_block_futura_flex_1.php
+++ shortcodes/td_block_futura_flex_1.php
@@ -10,14 +10,31 @@ class td_block_futura_flex_1 extends td_flex_block_1 {
     function inner($posts) {
 
         $buffy = '';
+
+        $MODULES_PER_ROW = 4;
+        $ROW_START = '<div class="td_block_futura_flex_1-row">' . "\n";
+        $ROW_END   = "\n" . '</div> <!-- td_block_futura_flex_1-row -->';
+
         $td_block_layout = new td_block_layout();
 
             if (!empty($posts)) {
+                $buffy .= $ROW_START;
+
+                $row_open = false;
+                $idx = 0;
+
                 foreach ($posts as $post) {
+                    if ($idx && ($idx % $MODULES_PER_ROW == 0)) {
+                        $buffy .= $ROW_END;
+                        $buffy .= $ROW_START;
+                    }
 
                     $td_module_flex_1 = new td_module_futura_flex_1($post, $this->get_all_atts(), TRUE);
                     $buffy .= $td_module_flex_1->render($post);
+
+                    $idx += 1;
                 }
+                $buffy .= $ROW_END;
             }
             $buffy .= $td_block_layout->close_all_tags();
 
